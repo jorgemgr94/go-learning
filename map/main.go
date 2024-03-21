60c01158e09da856712c5ef0239ec7c64d48f61a
Jorge García <jorge.garcia@zenbusiness.com>
2024-03-21 01:29:47 -0600

    feat: maps demo

---
+++ map/main.go
@@ -0,0 +1,35 @@
+package main
+
+import "fmt"
+
+func main() {
+	// declaring a map with var
+	// var colors2 map[string]string
+
+	// declaring a map with make
+	// colors3 := make(map[string]string)
+	// colors3["red"] = "#ff0000"
+
+	colors := map[string]string{
+		"red":   "#ff0000",
+		"green": "#4bf745",
+		"white": "#ffffff",
+	}
+
+	fmt.Println(colors)
+
+	// == manipulating a map ====================
+	// deleting a key from a map
+	delete(colors, "red")
+	fmt.Println(colors)
+
+	// == Iterating over a map ==================
+	printMap(colors)
+}
+
+func printMap(c map[string]string) {
+	// color => key, hex => value
+	for color, hex := range c {
+		fmt.Println("Hex code for", color, "is", hex)
+	}
+}
