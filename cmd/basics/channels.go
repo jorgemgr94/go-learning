ead56375eb8e509655cf87ccd6a8f359eecafcfe
Jorge García <jorgemgr94@gmail.com>
2025-09-12 23:24:43 -0600

    refactor(channels): improve concurrency handling and add alternative implementations

--- cmd/basics/channels.go
+++ cmd/basics/channels.go
@@ -14,38 +14,41 @@ func channels() {
 		"http://stackoverflow.com",
 		"http://golang.org",
 		"http://amazon.com",
-		// "https://lavelada.es",
 	}
 
 	// creating a channel
 	c := make(chan string)
 
+	// launch concurrent link checkers
 	for _, link := range links {
 		// concurrency is achieved by using go keyword.
 		go checkLink(link, c)
 	}
 
-	// this for loop purpose is to receive the message from the channel when the function is done.
-	// meaning that the function is done and the message is sent to the channel.
+	// Alternative 1: using range to receive the message from the channel
+	for l := range c {
+		time.Sleep(5 * time.Second)
+		go checkLink(l, c)
+	}
+
+	// Alternative 2: using a for loop to receive the message from the channel
 	// for {
+	// 	time.Sleep(5 * time.Second)
 	// 	go checkLink(<-c, c)
 	// }
 
-	// alternative way to write the above for loop
+	// Alternative 3: anonymous function to create a new go routine
 	// for l := range c {
-	// 	go checkLink(l, c)
+	// 	go func(link string) {
+	// 		time.Sleep(5 * time.Second)
+	// 		go checkLink(link, c)
+	// 	}(l)
 	// }
-
-	for l := range c {
-		// anonymous function: function literal to create a new go routine
-		go func(link string) {
-			time.Sleep(5 * time.Second)
-			go checkLink(link, c)
-		}(l)
-	}
 }
 
 func checkLink(link string, c chan string) {
+	fmt.Println("Checking link:", link, "at", time.Now())
+	// check if the link is up
 	_, err := http.Get(link)
 	if err != nil {
 		fmt.Println(link, "might be down!")
